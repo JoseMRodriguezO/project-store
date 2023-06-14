63a82d59800b5867e869cb9d99e6e49aa1085daf
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-06-14 15:51:03 -0500

    add products to the shop

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-
+import { PRODUCTS } from "../../products";
+import { Product } from "./Products";
 export const Shop = () => {
   return (
     <div className="shop">
       <div className="shoptitle">
         <h1>Country Fresh Finer Foods</h1>
       </div>
-      <div className="products"></div>
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
     </div>
   );
 };
