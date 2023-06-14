e55683599aff3a9d94020221a5654df759652319
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-06-14 15:29:54 -0500

    add data to products

---
+++ src/products.js
@@ -0,0 +1,58 @@
+import product1 from "./assets/images/Crackers.webp";
+import product2 from "./assets/images/icecoffe.webp";
+import product3 from "./assets/images/juice.webp";
+import product4 from "./assets/images/ketchup.webp";
+import product5 from "./assets/images/peanuts.webp";
+import product6 from "./assets/images/ravioli.webp";
+import product7 from "./assets/images/ruffles.webp";
+import product8 from "./assets/images/soda1.webp";
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Product 1",
+    price: 10.99,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Product 2",
+    price: 19.99,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Product 3",
+    price: 5.99,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Product 4",
+    price: 12.49,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Product 5",
+    price: 8.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Product 6",
+    price: 14.99,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Product 7",
+    price: 9.99,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Product 8",
+    price: 7.49,
+    productImage: product8,
+  },
+];
