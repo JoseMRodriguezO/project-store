e9da69ded489f9f439d4add0b472c49c2e8e8084
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-06-16 14:02:35 -0500

    Create subtotal for cart items and buttons

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -3,20 +3,29 @@ import { PRODUCTS } from "../../products";
 import { ShopContext } from "../../context/Shop-context";
 import { CartItem } from "./Cart-item";
 import "./cart.css";
-
+import { useNavigate } from "react-router-dom";
 export const Cart = () => {
-  const { cartItems } = useContext(ShopContext);
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+  const navigate = useNavigate;
   return (
     <div className="cart">
       <div>
-        <h1> Your Cart Items</h1>
+        <h1>Your Cart Items</h1>
       </div>
-      <div className="cartItems"></div>
-      {PRODUCTS.map((product) => {
-        if (cartItems[product.id] !== 0) {
-          return <CartItem data={product} />;
-        }
-      })}
+      <div className="cart">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+      <div className="checkout">
+        <p> Subtotal: ${totalAmount} </p>
+        <button> Continue Shopping </button>
+        <button>Checkout </button>
+      </div>
+      <h1> Your Shopping Cart is Empty</h1>
     </div>
   );
 };
