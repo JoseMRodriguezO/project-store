45f313fc0919d137533cebbb72d100d7efe043fa
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-06-16 13:15:04 -0500

    Add button for cartitems

--- src/pages/cart/Cart-item.jsx
+++ src/pages/cart/Cart-item.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/Shop-context";
 export const CartItem = (props) => {
   const { id, productName, price, productImage } = props.data;
-
+  const { cartItems } = useContext(ShopContext);
   return (
     <div className="cartItem">
       <img src={productImage} />
@@ -10,6 +10,11 @@ export const CartItem = (props) => {
         <b>{productName}</b>
       </p>
       <p>${price}</p>
+      <div className="countHandler">
+        <button> - </button>
+        <input value={cartItems[id]} />
+        <button> + </button>
+      </div>
     </div>
   );
 };
