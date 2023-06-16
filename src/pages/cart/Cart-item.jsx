74cf3b79ed8ed8413ef5a38d8bc37209ad23d287
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-06-16 13:28:50 -0500

    Update numbers of items in the cart

--- src/pages/cart/Cart-item.jsx
+++ src/pages/cart/Cart-item.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { ShopContext } from "../../context/Shop-context";
 export const CartItem = (props) => {
   const { id, productName, price, productImage } = props.data;
-  const { cartItems, addToCart, removeFromCart } = useContext(ShopContext);
+  const { cartItems, addToCart, removeFromCart, updateCartItemCount } = useContext(ShopContext);
   return (
     <div className="cartItem">
       <img src={productImage} />
@@ -12,7 +12,7 @@ export const CartItem = (props) => {
       <p>${price}</p>
       <div className="countHandler">
         <button onClick={() => removeFromCart(id)}> - </button>
-        <input value={cartItems[id]} />
+        <input value={cartItems[id]} onChange={(e) => updateCartItemCount(Number(e.target.value), id)} />
         <button onClick={() => addToCart(id)}> + </button>
       </div>
     </div>
